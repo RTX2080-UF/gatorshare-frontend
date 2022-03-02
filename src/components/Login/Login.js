09633975f20929c56212c81b3d6dffdea0f51872
valasaladinesh <valasaladinesh@gmail.com>
2022-03-01 22:19:49 -0500

    Created basic UI for Login component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,25 @@
+import {Button, Form} from 'react-bootstrap';
+
 const Login = () => {
-    return <div></div>
+    return <div className="main-container">
+        <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>User Name:</Form.Label>
+                <Form.Control type="email" placeholder="Enter Username" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Keep me signed in" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Login
+            </Button>
+        </Form>
+    </div>
 } 
 
 export default Login

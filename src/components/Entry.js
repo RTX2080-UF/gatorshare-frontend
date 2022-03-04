5153e829dc82ff6fd8ed1758b5e0866e07c2b630
valasaladinesh <valasaladinesh@gmail.com>
2022-03-03 23:30:18 -0500

    Signup component added

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Login from "./Login/Login"
-// import Signup from "./Signup/Signup"
+import Signup from "./Signup/Signup"
 import Main from "./Main";
 
 const Entry = () => {
@@ -9,7 +9,7 @@ const Entry = () => {
         <BrowserRouter>
             <Routes>
                 <Route path="/login" element={<Login />} />
-                {/* <Route path="/signup" element={<Signup />} /> */}
+                <Route path="/signup" element={<Signup />} />
                 <Route path="/" element={<Main />} />
             </Routes>
         </BrowserRouter>

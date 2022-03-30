242c97000fd0947662aaf2590d18ad10342504d7
valasaladinesh <valasaladinesh@gmail.com>
2022-03-30 00:18:22 -0400

    fixing netlify errors

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -2,7 +2,7 @@ import { Button, Form } from 'react-bootstrap';
 import BGImage from "../BGImage"
 import data from "../../data/Data"
 import { useState } from "react"
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 // import {setAccessToken} from '../../utils/SessionUtils'
 
 const Signup = () => {

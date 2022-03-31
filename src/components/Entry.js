d402cf4a1e5fb0377caf373c2a05c76ac1c44ee9
valasaladinesh <valasaladinesh@gmail.com>
2022-03-31 17:09:08 -0400

    forgot passcode section added

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -2,6 +2,7 @@ import React from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Onboarding from "../pages/Onboarding";
 import Login from "./Login/Login"
+import ForgotPassword from "./Login/ForgotPassword"
 import Signup from "./Signup/Signup"
 import Settings from "./Settings/Settings"
 import Main from "./Main";
@@ -12,6 +13,7 @@ const Entry = () => {
         <BrowserRouter>
             <Routes>
                 <Route path="/login" element={<Login />} />
+                <Route path="/forgotPwd" element={<ForgotPassword />} />
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/" element={<Main />} />
                 <Route path="/settings" element={<Settings />} />

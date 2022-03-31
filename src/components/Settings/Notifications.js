df52810709d61cc8ca8cd3b5165d721c5853989f
valasaladinesh <valasaladinesh@gmail.com>
2022-03-31 16:46:21 -0400

    Adding email alert toggle

--- src/components/Settings/Notifications.js
+++ src/components/Settings/Notifications.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import { Col, Row } from "react-bootstrap"
 import Data from "../../data/Data"
 import { setUser } from "../../utils/SessionUtils"
+import { Form } from 'react-bootstrap'
 
 
 const Notifications = () => {
@@ -15,7 +16,10 @@ const Notifications = () => {
     return <div className="page bg-light ps-5">
         <Row>
             <h2 className="mb-3">Notifications</h2>
-            
+            <div className="form-check form-switch">
+                <input className="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked/>
+                <label className="form-check-label" for="flexSwitchCheckChecked">Recieve email alerts</label>
+            </div>
         </Row>
     </div>
 }

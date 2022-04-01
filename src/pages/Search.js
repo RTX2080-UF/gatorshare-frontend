711a5cb30b2183d24ba7f65505f42cc8d935120e
Rishabh Tatiraju <me@rishabh.blog>
2022-04-01 15:36:56 -0400

    Added Search Filters modal and plugged it into Search page. Changed filter icon in the searchbar

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,8 +1,10 @@
 import { mdiArrowLeft } from "@mdi/js"
 import Icon from "@mdi/react"
+import { mdiFilterVariant } from '@mdi/js'; 
 import { useState } from "react"
 import { Col, Form, Nav, Navbar, Row } from "react-bootstrap"
 import emptySearchIcon from '../assets/search-splash.png'
+import SearchFilter from "../components/modals/SearchFilter/SearchFilter"
 
 import COLORS from '../theme/colors'
 
@@ -10,8 +12,27 @@ const Search = () => {
 
     const [searchQuery, setSearchQuery] = useState('')
 
+    const [showFilterModal, setShowFilterModal] = useState(false)
+
+    const [filter, setFilter] = useState()
+    const [filterValue, setFilterValue] = useState()
+    const [sort, setSort] = useState()
+
     const handleSearchQueryChange = (event) => {
         setSearchQuery(event.target.value)
+    }
+
+    const onSelectFilters = (filter, filterValue, sort) => {
+        if (filter && filterValue) {
+            setFilter(filter)
+            setFilterValue(filterValue)
+        }
+
+        if(sort) {
+            setSort(sort)
+        }
+        
+        setShowFilterModal(false)
     }
 
     const pageStyle = {
@@ -28,25 +49,30 @@ const Search = () => {
                 <Form.Control type='text' placeholder='Search' autoFocus onChange={handleSearchQueryChange} />
             </Form.Group>
 
-            <Nav className="ms-auto mt-auto">
-                <p className="me-4"><b>Filter</b> 0 applied</p>
-                <p className="me-4"><b>Sort</b> by relevance</p>
+            <Nav className="ms-auto mt-auto mb-auto me-3" onClick={() => setShowFilterModal(true)}>
+                <Icon path={mdiFilterVariant} size={1} color={COLORS.accent} />
             </Nav>
         </Navbar>
 
         <Row className='page m-0 p-0' style={pageStyle}>
             {
                 searchQuery === '' ?
-                    <Col xs={12} md={{span: 4, offset: 4}} className='text-center p-5 mt-auto mb-auto'>
-                        <img alt='An icon with magnifying glass and some documents' src={emptySearchIcon} width='25%'/>
+                    <Col xs={12} md={{ span: 4, offset: 4 }} className='text-center p-5 mt-auto mb-auto'>
+                        <img alt='An icon with magnifying glass and some documents' src={emptySearchIcon} width='25%' />
                         <h4 className="mt-4">Start Typing Something</h4>
                         <p>Posts that match your search query will appear here.</p>
                     </Col> :
                     <Col xs={12}>
-
+                        
                     </Col>
             }
         </Row>
+
+        <SearchFilter
+            show={showFilterModal}
+            onSelect={onSelectFilters}
+            onClose={() => setShowFilterModal(false)} />
+
     </div>
 }
 

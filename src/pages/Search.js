fd840f7c0388fe84ee2804ba43d64a6117e6f496
Rishabh Tatiraju <me@rishabh.blog>
2022-04-01 12:29:06 -0400

    CSS changes and finalizing filter and sort layout

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,41 +1,51 @@
 import { mdiArrowLeft } from "@mdi/js"
 import Icon from "@mdi/react"
-import { Col, Button, Form, Nav, Navbar, Row } from "react-bootstrap"
-import COLORS from "../theme/colors"
+import { useState } from "react"
+import { Col, Form, Nav, Navbar, Row } from "react-bootstrap"
+import emptySearchIcon from '../assets/search-splash.png'
+
+import COLORS from '../theme/colors'
 
 const Search = () => {
 
-    const style = {
-        backgroundColor: COLORS.background.sidebar,
-        height: '100%'
+    const [searchQuery, setSearchQuery] = useState('')
+
+    const handleSearchQueryChange = (event) => {
+        setSearchQuery(event.target.value)
     }
 
-    const topBarStyle = {
+    const pageStyle = {
         backgroundColor: COLORS.background.base,
-        height: '72px'
     }
 
-    return <div className="page p-0 m-0">
-        <Navbar>
+    return <div className="page-container p-0 m-0">
+        <Navbar className="topbar">
             <Navbar.Brand>
                 <a href="/"><Icon path={mdiArrowLeft} size={1} color="gray" className="m-3" /></a>
             </Navbar.Brand>
 
             <Form.Group className='me-auto mt-3 ms-4'>
-                <Form.Control type='text' placeholder='Search' autoFocus/>
+                <Form.Control type='text' placeholder='Search' autoFocus onChange={handleSearchQueryChange} />
             </Form.Group>
 
-            <Nav className="ms-auto">
-                <Button variant="warning" className="me-3 gatorshare-button">Filter</Button>
-                <Button variant="warning" className="me-3 gatorshare-button">Sort</Button>
+            <Nav className="ms-auto mt-auto">
+                <p className="me-4"><b>Filter</b> 0 applied</p>
+                <p className="me-4"><b>Sort</b> by relevance</p>
             </Nav>
         </Navbar>
-        
 
-        <Row className='main-container m-0 p-0'>
-            <Col xs={12} md={8} lg={}>
+        <Row className='page m-0 p-0' style={pageStyle}>
+            {
+                searchQuery === '' ?
+                    <Col xs={12} md={{span: 4, offset: 4}} className='text-center p-5 mt-auto mb-auto'>
+                        <img alt='An icon with magnifying glass and some documents' src={emptySearchIcon} width='25%'/>
+                        <h4 className="mt-4">Start Typing Something</h4>
+                        <p>Posts that match your search query will appear here.</p>
+                    </Col> :
+                    <Col xs={12}>
 
-            </Col>
+                    </Col>
+            }
         </Row>
     </div>
 }

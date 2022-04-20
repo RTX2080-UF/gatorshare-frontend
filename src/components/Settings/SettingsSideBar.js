af680e90b3a6810d24649714ed1484d7d7ff0012
valasaladinesh <valasaladinesh@gmail.com>
2022-04-20 19:00:28 -0400

    settings side bar bug resolved

--- src/components/Settings/SettingsSideBar.js
+++ src/components/Settings/SettingsSideBar.js
@@ -1,7 +1,7 @@
 import { mdiAccountCogOutline } from '@mdi/js';
 import { mdiShieldAccountOutline } from '@mdi/js';
 import { mdiBellOutline } from '@mdi/js';
-import React from "react"
+import React, { useState } from "react"
 import { Col, Row, Button } from 'react-bootstrap';
 import COLORS from '../../theme/colors';
 import SettingsSideBarItem from './SettingsSideBarItem';
@@ -12,13 +12,25 @@ import Colors from '../../theme/colors'
 
 const SettingsSideBar = (props) => {
     const navigate = useNavigate();
+    const [pColor, setPColor] = useState(Colors.accent);
+    const [sColor, setSColor] = useState(Colors.text.secondary);
+    const [nColor, setNColor] = useState(Colors.text.secondary);
     const handleProfileClick = (e)=> {
+        setPColor(Colors.accent);
+        setSColor(Colors.text.secondary);
+        setNColor(Colors.text.secondary);
         props.setSettingsPath(PATH.PROFILE);
     }
     const handleSecurityClick = (e)=> {
+        setSColor(Colors.accent);
+        setPColor(Colors.text.secondary);
+        setNColor(Colors.text.secondary);
         props.setSettingsPath(PATH.SECURITY);
     }
     const handleNotificationsClick = (e)=> {
+        setNColor(Colors.accent);
+        setSColor(Colors.text.secondary);
+        setPColor(Colors.text.secondary);
         props.setSettingsPath(PATH.NOTIFICATIONS);
     }
     const handleLogoutClick = (e)=> {
@@ -34,13 +46,13 @@ const SettingsSideBar = (props) => {
     return <div style={style} className={"m-0 p-0 pt-5"}>
             <Row className={"m-0 p-0 pt-5"}>
                 <Col xs={12} className={"m-0 p-0"} id="profile" key="Profile">
-                    <SettingsSideBarItem name="Profile" icon={mdiAccountCogOutline} active handleClick={(e)=>handleProfileClick(e)}/>
+                    <SettingsSideBarItem name="Profile" icon={mdiAccountCogOutline} active pcolor={pColor} handleClick={(e)=>handleProfileClick(e)}/>
                 </Col>
                 <Col xs={12} className={"m-0 p-0"} id="security" key="Security">
-                    <SettingsSideBarItem name="Security" icon={mdiShieldAccountOutline} active handleClick={(e)=>handleSecurityClick(e)}/>
+                    <SettingsSideBarItem name="Security" icon={mdiShieldAccountOutline} active pcolor={sColor} handleClick={(e)=>handleSecurityClick(e)}/>
                 </Col>
                 <Col xs={12} className={"m-0 p-0"} id="notifications" key="Notifications">
-                    <SettingsSideBarItem name="Notifications" icon={mdiBellOutline} active handleClick={(e)=>handleNotificationsClick(e)}/>
+                    <SettingsSideBarItem name="Notifications" icon={mdiBellOutline} active pcolor={nColor} handleClick={(e)=>handleNotificationsClick(e)}/>
                 </Col>
             </Row>
             <div className='text-align-center logout-div'>

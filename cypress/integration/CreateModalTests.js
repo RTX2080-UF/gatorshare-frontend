0edab377e6891f48fef77e10a3c8c694e9727c89
valasaladinesh <valasaladinesh@gmail.com>
2022-04-20 18:43:29 -0400

    Create modal cypress testcases

--- cypress/integration/CreateModalTests.js
+++ cypress/integration/CreateModalTests.js
@@ -13,14 +13,41 @@ describe('Testing Create Modal UI', function () {
         cy.wait(3000)
     });
 
-    it('click on create', function () {
+    it('create modal header title check', function () {
         cy.visit("/");
         cy.get("#createModal").click();
+        cy.get(".h4").should('contain', 'Create post');
+        cy.get("#ctitle").type('hello').should('have.value','hello');
+        cy.get("#cdesc").type('hello').should('have.value','hello');
     });
-    it('Test the back link', function () {
-        cy.visit("/forgotPwd");
-        cy.get('a').click();
-        cy.location('pathname').should('eq', '/login');
+    
+    it('create Modal title and description tests', function () {
+        cy.visit("/");
+        cy.get("#createModal").click();
+        cy.get("#ctitle").type('hello').should('have.value','hello');
+        cy.get("#cdesc").type('hello').should('have.value','hello');
+    });
+
+    it('create Modal limit and date tests', function () {
+        cy.visit("/");
+        cy.get("#createModal").click();
+        cy.get("#ctitle").type('hello').should('have.value','hello');
+        cy.get("#cdesc").type('hello').should('have.value','hello');
+        cy.get("#cdate").type('2022-12-12').should('have.value','2022-12-12');
+        cy.get("#climit").type(2).should('have.value',2);
+    });
+
+    it('create tags tests', function () {
+        cy.visit("/");
+        cy.get("#createModal").click();
+        cy.get("#ctitle").type('hello').should('have.value','hello');
+        cy.get("#cdesc").type('hello').should('have.value','hello');
+        cy.get("#cdate").type('2022-12-12').should('have.value','2022-12-12');
+        cy.get("#climit").type(2).should('have.value',2);
+        cy.get("#ctag").type("hello").type('{enter}');
+        cy.get('.badge').should('contain', 'hello');
+        cy.get("#ctag").type("hello1").type('{enter}');
+        cy.get('.badge').should('contain', 'hello1');
     });
 
 });

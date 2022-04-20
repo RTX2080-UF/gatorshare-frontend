5bbd6825ee8717f8b40496f8b2b927ee36154564
valasaladinesh <valasaladinesh@gmail.com>
2022-04-19 22:49:21 -0400

    create post integration changes

--- src/components/CreatePostModal.js
+++ src/components/CreatePostModal.js
@@ -1,14 +1,20 @@
 import { Modal, Button, Form, Badge, Dropdown} from 'react-bootstrap';
 import { React, useState } from 'react';
+import data from "../data/Data";
 import "../App.css";
 
 const CreatePostModal = (props) => {
     const [checked, setChecked] = useState(false);
     const [tag, setTag] = useState("");
     const [title, setTitle] = useState("");
+    const [desc, setDesc] = useState("");
+    const [dropdown, setDropdown] = useState("");
+    const [date, setDate] = useState(new Date());
+    const [limit, setLimit] = useState();
     const [tags, setTags] = useState([]);
     const handleChecked = (e) => {
         setChecked(e.target.checked)
+        setLimit(0);
     }
 
     const handleTagsEnter = () => {
@@ -17,7 +23,16 @@ const CreatePostModal = (props) => {
         setTag("");    
     }
     const createPost = () => {
-
+        console.log("date.getTime - ", new Date(date).getTime())
+        // const requestData = `{"title":"${title}","description":"${desc}","userLimit":${limit},"status":1,"tags":"${tags}","expiry":"${new Date(date).getTime()}"}`;
+        const requestData = JSON.stringify({title: title, description: desc, userLimit: parseInt(limit), status: 1, tags: tags, expiry: new Date(date).getTime()})
+        console.log(requestData)
+        
+        data.createPost(requestData).then(res => {
+            console.log("response - ", res);
+            // navigate("/login");
+            props.handleClose();
+        })
     }
     
     return (
@@ -38,13 +53,13 @@ const CreatePostModal = (props) => {
                         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                             <Form.Control type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)}/>
                             {/* <Form.Label>Description</Form.Label> */}
-                            <Form.Control as="textarea" rows={3} placeholder='Enter description' />
+                            <Form.Control as="textarea" rows={3} placeholder='Enter description' value={desc} onChange={(e) => setDesc(e.target.value)}/>
                             <div className="row">
                                 <div className="col-lg-6 col-md-12">
                                     <p>Expiry: </p>
                                 </div>
                                 <div className="col-lg-6 col-md-12">
-                                    <Form.Control type="date"/>
+                                    <Form.Control type="date" value={date} onChange={(e) => setDate(e.target.value)}/>
                                 </div>
                             </div>
                             <div className="row">
@@ -52,7 +67,7 @@ const CreatePostModal = (props) => {
                                     <p>Participant Limit: </p>
                                 </div>
                                 <div className="col-lg-6 col-md-12 d-flex flex-row">
-                                    <Form.Control className="me-5" type="number" disabled={checked === true ? true : false}/>
+                                    <Form.Control className="me-5" type="number" disabled={checked === true ? true : false} value={limit} onChange={(e) => setLimit(e.target.value)}/>
                                     <Form.Check className="ms-3 me-2"label="Unlimited" onClick={handleChecked}/>
                                 </div>
                             </div>
@@ -61,14 +76,13 @@ const CreatePostModal = (props) => {
                                     <p>Category: </p>
                                 </div>
                                 <div className="col-lg-6 col-md-12">
-                                    <Dropdown>
-                                        <Dropdown.Toggle variant="secondary" className="primarycolor" id="dropdown-basic" title="Select">Select Category</Dropdown.Toggle>
-                                        <Dropdown.Menu>
-                                            <Dropdown.Item eventKey="1">Cab share</Dropdown.Item>
-                                            <Dropdown.Item eventKey="2">Groceries</Dropdown.Item>
-                                            <Dropdown.Item eventKey="3">Vacation</Dropdown.Item>
-                                        </Dropdown.Menu>
-                                    </Dropdown>
+                                    <Form.Select aria-label="Default select">
+                                        <option>Select Category</option>
+                                        <option value="1">Cab share</option>
+                                        <option value="2">Groceries</option>
+                                        <option value="3">Vacation</option>
+                                        <option value="4">Meetup</option>
+                                    </Form.Select>
                                 </div>
                             </div>
                             <div className="row mt-3">
@@ -92,7 +106,7 @@ const CreatePostModal = (props) => {
                     <Button variant="secondary" onClick={()=>props.handleClose()}>
                         Cancel
                     </Button>
-                    <Button  className='primarycolor'onClick={()=>createPost()}>Post</Button>
+                    <Button  className='primarycolor' onClick={()=>createPost()}>Post</Button>
                 </Modal.Footer>
             </Modal>
              

1715f6aa7326e74d53fa1c7117fba5b146a22365
valasaladinesh <valasaladinesh@gmail.com>
2022-04-20 19:02:51 -0400

    settings side bar click issue resolved

--- src/components/Settings/SettingsSideBarItem.js
+++ src/components/Settings/SettingsSideBarItem.js
@@ -5,7 +5,7 @@ const SettingsSideBarItem = ({ icon, name, active, pcolor, handleClick }) => {
     const highlightColor = active ? Colors.accent : Colors.text.secondary
     const backgroundColor = active ? Colors.background.base : "transparent"
 
-    return <div className="p-3" onClick={handleClick} style={{backgroundColor: backgroundColor}}>
+    return <div className="p-3 pointer" onClick={handleClick} style={{backgroundColor: backgroundColor}}>
             <Icon path={icon} size={1} color={pcolor} />
             {'   '}
             <b style={{ color: pcolor}}>{name}</b>
